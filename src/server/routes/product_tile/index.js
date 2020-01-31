7374efda50aaf5ea1da12ca7d50f65de7e16aecc
Oleksii Shalyn <pipichindreks.xxx@gmail.com>
2020-01-31 11:29:16 +0200

    feat(Product tile): Added product helper

--- src/server/routes/product_tile/index.js
+++ src/server/routes/product_tile/index.js
@@ -1,25 +1,56 @@
-const config = require('../../config');
-const req = require("request");
-const axios = require('axios');
+const config = require("../../config");
+const axios = require("axios");
+const token = require("../../config/auth");
+const {
+  getReadyProducts,
+  getProductListPrice,
+  getProductSwatches
+} = require("../../helpers/product/productTileHelper");
 
 module.exports = (fastify, opts, done) => {
-  fastify.get('/producttile/:category', async (request, reply) => {
-    const options = (url, queryString) =>  {
+  fastify.get("/producttile/:category", async (request, reply) => {
+    const options = (url, queryString, method, body) => {
       return {
-      method: "GET",
-      url: config.url + url + (queryString ? '?' + queryString : ""),
-      json: true,
-      headers: {
-        "Content-Type": "application/json",
-        "x-dw-client-id": config.client_id
-      }
-    }
+        method: method,
+        url: config.url + url + (queryString ? "?" + queryString : ""),
+        json: true,
+        headers: {
+          "Content-Type": "application/json",
+          "x-dw-client-id": config.client_id,
+          Authorization: "Bearer " + token.get()
+        }
+      };
     };
-    let result = await axios(options('/product_search', 'expand=images&refine=cgid=' + request.params.category));
-    let productSearchHits = result.data.hits;
+    const productSearch_result = await axios(
+      options(
+        "/product_search",
+        "count=24&refine=cgid=" + request.params.category
+      )
+    );
+    const productSearchHits = productSearch_result.data.hits;
+    const masterProductIds = productSearchHits
+      .map(hit => hit.product_id)
+      .toString();
+    await axios.post("http://127.0.0.1:8000/customers/auth", { type: "guest" });
+    const products_result = await axios(
+      options(
+        `/products/(${masterProductIds})`,
+        "expand=variations,images,prices",
+        "GET"
+      )
+    );
+    const products = products_result.data.data;
 
+    const detailedProducts = await getReadyProducts(products);
 
-    reply.code(200).send(productSearchHits);
+    const readyProducts = detailedProducts.map(product => {
+      let tempProduct = {...product};
+      tempProduct.listPrices = getProductListPrice(tempProduct.variants);
+      tempProduct.images = getProductSwatches(tempProduct.variants);
+      return tempProduct;
+    });
+
+    reply.code(200).send(readyProducts);
   });
-  done()
-}
+  done();
+};
