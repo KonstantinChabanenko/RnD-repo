657f5cbef412d4ffcb23569d764fbc4836d4b844
Чабаненко Константин <memchabanenko@gmail.com>
2020-02-06 00:03:43 +0200

    refactor(Refinement Bar): Was created a general function that applies filters

--- src/client/components/RefinementBar/Colors.jsx
+++ src/client/components/RefinementBar/Colors.jsx
@@ -1,57 +1,28 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import applyRefinement from './applyRefinement';
 
-const Colors = ({ colors, selectedRefinements, setSelectedRefinements }) => {
-    const applyRefinement = (e) => {
-        e.stopPropagation();
-        const selectedColorValue = e.currentTarget.dataset.colorAttribute;
-        const selectedColor = selectedRefinements.colors ? selectedRefinements.colors.find(colorValue => colorValue === selectedColorValue) : null;
-        if (!selectedColor) {
-            setSelectedRefinements(prevState => {
-                const updatedColors = prevState.colors ? prevState.colors : [];
-                updatedColors.push(selectedColorValue);
-
-                return ({
-                    ...prevState,
-                    colors: updatedColors
-                })
-            });
-        } else {
-            setSelectedRefinements(prevState => {
-                const updatedColors = prevState.colors;
-                const selectedColorIndex = updatedColors.indexOf(selectedColor);
-                updatedColors.splice(selectedColorIndex, 1);
-
-                return ({
-                    ...prevState,
-                    colors: updatedColors
-                })
-            });
-        }
-    }
-
-    return (
-        <Card className="refinement colors">
-            <Card.Header>{colors.label}</Card.Header>
-            <Card.Body>
-                <ul className="refinement__values top-level">
-                    {colors.values.map(color => (
-                        <li key={color.value} className="color-attribute">
-                            <div className="swatch-wrapper"
-                                data-color-attribute={color.value}
-                                onClick={(e) => applyRefinement(e)}
-                            >
-                                <span
-                                    className={`value swatch-circle swatch-circle-${color.presentation_id}${color.hit_count === 0 ? ' disabled' : ''}`}
-                                ></span>
-                                {selectedRefinements.colors && selectedRefinements.colors.some(selectedColor => selectedColor === color.value) ? <span className="checked-mark"></span> : null}
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </Card.Body>
-        </Card>
-    )
-}
+const Colors = ({ colors, selectedRefinements, setSelectedRefinements }) => (
+    <Card className="refinement colors">
+        <Card.Header>{colors.label}</Card.Header>
+        <Card.Body>
+            <ul className="refinement__values top-level">
+                {colors.values.map(color => (
+                    <li key={color.value} className="color-attribute">
+                        <div className="swatch-wrapper"
+                            data-filter-value={color.value}
+                            onClick={(e) => applyRefinement(e, selectedRefinements, setSelectedRefinements, "colors")}
+                        >
+                            <span
+                                className={`value swatch-circle swatch-circle-${color.presentation_id}${color.hit_count === 0 ? ' disabled' : ''}`}
+                            ></span>
+                            {selectedRefinements.colors && selectedRefinements.colors.some(selectedColor => selectedColor === color.value) ? <span className="checked-mark"></span> : null}
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </Card.Body>
+    </Card>
+)
 
 export default Colors;
--- src/client/components/RefinementBar/Sizes.jsx
+++ src/client/components/RefinementBar/Sizes.jsx
@@ -1,51 +1,25 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import applyRefinement from './applyRefinement';
 
-const Sizes = ({ sizes, selectedRefinements, setSelectedRefinements }) => {
-    const applyRefinement = (e) => {
-        e.stopPropagation();
-        const selectedSizeValue = e.currentTarget.dataset.sizeValue;
-        const selectedSize = selectedRefinements.sizes ? selectedRefinements.sizes.find(sizeValue => sizeValue === selectedSizeValue) : null;
-
-        if (!selectedSize) {
-            setSelectedRefinements(prevState => {
-                const updatedColors = prevState.colors ? prevState.colors : [];
-                updatedColors.push(selectedSizeValue);
-
-                return ({
-                    ...prevState,
-                    sizes: updatedColors
-                })
-            });
-        } else {
-            setSelectedRefinements(prevState => {
-                const updatedSizes = prevState.colors;
-                const selectedSizeIndex = updatedSizes.indexOf(selectedSize);
-                updatedSizes.splice(selectedSizeIndex, 1);
-
-                return ({
-                    ...prevState,
-                    sizes: updatedSizes
-                })
-            });
-        }
-    }
-
-    return (
-        <Card className="refinement colors">
-            <Card.Header>{sizes.label}</Card.Header>
-            <Card.Body>
-                <ul className="refinement__values top-level">
-                    {sizes.values.map(size => (
-                        <li key={size.value} className="value" data-size-value={size.value} onClick={(e) => applyRefinement(e)}>
-                            {selectedRefinements.sizes && selectedRefinements.sizes.some(selectedSize => selectedSize === size.value) ? <i className="fas fa-check-square"></i> : <i className="far fa-square"></i>}
-                            <span className="category-name">{size.label}</span>
-                        </li>
-                    ))}
-                </ul>
-            </Card.Body>
-        </Card>
-    )
-}
+const Sizes = ({ sizes, selectedRefinements, setSelectedRefinements }) => (
+    <Card className="refinement sizes">
+        <Card.Header>{sizes.label}</Card.Header>
+        <Card.Body>
+            <ul className="refinement__values top-level">
+                {sizes.values.map(size => (
+                    <li
+                        key={size.value}
+                        className="value"
+                        data-filter-value={size.value}
+                        onClick={(e) => applyRefinement(e, selectedRefinements, setSelectedRefinements, "sizes")}>
+                        {selectedRefinements.sizes && selectedRefinements.sizes.some(selectedSize => selectedSize === size.value) ? <i className="fas fa-check-square"></i> : <i className="far fa-square"></i>}
+                        <span className="category-name">{size.label}</span>
+                    </li>
+                ))}
+            </ul>
+        </Card.Body>
+    </Card>
+)
 
 export default Sizes;
