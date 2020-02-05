514cc3dcc01c9f4b4f297b595918971ce36b9b7b
Чабаненко Константин <memchabanenko@gmail.com>
2020-02-05 21:58:44 +0200

    feature(Refinement Bar): - Color refinements were completed

--- src/client/components/RefinementBar/Colors.jsx
+++ src/client/components/RefinementBar/Colors.jsx
@@ -4,12 +4,23 @@ import { Card } from 'react-bootstrap';
 const Colors = ({ colors, selectedRefinements, setSelectedRefinements }) => {
     const applyRefinement = (e) => {
         e.stopPropagation();
-        const selectedColor = e.currentTarget.dataset.colorAttribute;
-        const isSelected = selectedRefinements.colors ? selectedRefinements.colors.some(color => color === selectedColor) : false;
-        if (!isSelected) {
+        const selectedColorValue = e.currentTarget.dataset.colorAttribute;
+        const selectedColor = selectedRefinements.colors ? selectedRefinements.colors.find(colorValue => colorValue === selectedColorValue) : null;
+        if (!selectedColor) {
             setSelectedRefinements(prevState => {
                 const updatedColors = prevState.colors ? prevState.colors : [];
-                updatedColors.push(selectedColor);
+                updatedColors.push(selectedColorValue);
+
+                return ({
+                    ...prevState,
+                    colors: updatedColors
+                })
+            });
+        } else {
+            setSelectedRefinements(prevState => {
+                const updatedColors = prevState.colors;
+                const selectedColorIndex = updatedColors.indexOf(selectedColor);
+                updatedColors.splice(selectedColorIndex, 1);
 
                 return ({
                     ...prevState,
@@ -26,11 +37,15 @@ const Colors = ({ colors, selectedRefinements, setSelectedRefinements }) => {
                 <ul className="refinement__values top-level">
                     {colors.values.map(color => (
                         <li key={color.value} className="color-attribute">
-                            <span
-                                className={`value swatch-circle swatch-circle-${color.presentation_id}${color.hit_count === 0 ? ' disabled' : ''}`}
+                            <div className="swatch-wrapper"
                                 data-color-attribute={color.value}
                                 onClick={(e) => applyRefinement(e)}
-                            ></span>
+                            >
+                                <span
+                                    className={`value swatch-circle swatch-circle-${color.presentation_id}${color.hit_count === 0 ? ' disabled' : ''}`}
+                                ></span>
+                                {selectedRefinements.colors && selectedRefinements.colors.some(selectedColor => selectedColor === color.value) ? <span className="checked-mark"></span> : null}
+                            </div>
                         </li>
                     ))}
                 </ul>
