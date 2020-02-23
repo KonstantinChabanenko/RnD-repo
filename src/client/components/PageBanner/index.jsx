d408c8db566314f9b8a569b46fd35ff87f5a971b
Чабаненко Константин <memchabanenko@gmail.com>
2020-02-23 21:44:23 +0200

    fix(Page Banner): Fix for page banner

--- src/client/components/PageBanner/index.jsx
+++ src/client/components/PageBanner/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const PageBanner = ({ banner }) => (
+const PageBanner = ({ banner }) => banner ? (
     <div
         className="page-banner position-relative"
         style={{ backgroundImage: `url(${banner.imageSrc})` }}
     >
         <h1 className="page-banner__title position-absolute m-0">{banner.title}</h1>
     </div>
-)
+) : null;
 
 export default PageBanner;
