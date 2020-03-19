ca89d1680beeca1ed8ec034e76c2eb43b9be8d28
Чабаненко Константин <memchabanenko@gmail.com>
2020-03-19 13:05:17 +0200

    feature(basket): Authorization was removed from header in post request

--- src/client/services/http.js
+++ src/client/services/http.js
@@ -2,72 +2,92 @@ import api from '../config/api';
 import axios from 'axios';
 
 const { root } = api;
-let authToken = null;
+// let authToken = null;
 
-const authTokenRequestPost = (type) => axios.post(
-    `${root}/customers/auth`,
-    {
-        type,
-    },
-).then(res => res.data.authorization);
+// const authTokenRequestPost = (type) => axios.post(
+//     `${root}/customers/auth`,
+//     {
+//         type,
+//     },
+// ).then(res => res.data.authorization);
 
-const checkAuthTokenExpiration = (res) => {
-    if (res.fault && res.fault.type === "ExpiredTokenException") {
-        authToken = authTokenRequestPost("refresh");
-        return true;
-    } else {
-        return false;
-    }
-}
+// const checkAuthTokenExpiration = (res) => {
+//     if (res.fault && res.fault.type === "ExpiredTokenException") {
+//         authToken = authTokenRequestPost("refresh");
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-const requestGet = (path, params) => axios.get(
+// const requestGet = (path, params) => axios.get(
+//     `${root}/${path}`,
+//     {
+//         headers: {
+//             "x-dw-client-id": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+//             Authorization: authToken,
+//         },
+//         params,
+//     }
+// ).then(res => res.data).catch(err => err.response.data);
+
+// export const get = async (path, params) => {
+//     if (!authToken) {
+//         authToken = await authTokenRequestPost("guest");
+//     }
+
+//     let res = await requestGet(path, params);
+//     const authTokenHasExpired = checkAuthTokenExpiration(res);
+
+//     if (authTokenHasExpired) {
+//         res = requestGet(path, params);
+//     }
+
+//     return res;
+// }
+
+// const requestPost = (path, data) => axios.post(
+//     `${root}/${path}`,
+//     data,
+//     {
+//         headers: {
+//             "x-dw-client-id": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+//             Authorization: authToken,
+//         }
+//     }
+// ).then(res => res.data).catch(err => err);
+
+// export const post = async (path, data) => {
+//     if (!authToken) {
+//         authToken = await authTokenRequestPost("guest");
+//     }
+
+//     let res = await requestPost(path, data);
+//     const authTokenHasExpired = checkAuthTokenExpiration(res);
+
+//     if (authTokenHasExpired) {
+//         res = requestPost(path, data);
+//     }
+
+//     return res;
+// }
+
+export const get = (path, params) => axios.get(
     `${root}/${path}`,
     {
         headers: {
             "x-dw-client-id": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            Authorization: authToken,
         },
         params,
     }
 ).then(res => res.data).catch(err => err.response.data);
 
-export const get = async (path, params) => {
-    if (!authToken) {
-        authToken = await authTokenRequestPost("guest");
-    }
-
-    let res = await requestGet(path, params);
-    const authTokenHasExpired = checkAuthTokenExpiration(res);
-
-    if (authTokenHasExpired) {
-        res = requestGet(path, params);
-    }
-
-    return res;
-}
-
-const requestPost = (path, data) => axios.post(
+export const post = (path, data) => axios.post(
     `${root}/${path}`,
     data,
     {
         headers: {
             "x-dw-client-id": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            Authorization: authToken,
         }
     }
 ).then(res => res.data).catch(err => err);
-
-export const post = async (path, data) => {
-    if (!authToken) {
-        authToken = await authTokenRequestPost("guest");
-    }
-
-    let res = await requestPost(path, data);
-    const authTokenHasExpired = checkAuthTokenExpiration(res);
-
-    if (authTokenHasExpired) {
-        res = requestPost(path, data);
-    }
-
-    return res;
-}
