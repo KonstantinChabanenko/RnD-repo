03d3b6d74a0444470dd5da514544f1a845f4dba5
Oleksii Shalyn <pipichindreks.xxx@gmail.com>
2020-03-19 14:19:03 +0200

    refactor(BD apiHelper): added support for POST w/o body

--- src/server/routes/auth/index.js
+++ src/server/routes/auth/index.js
@@ -1,16 +1,29 @@
-const token = require('../../config/auth');
-const {apiPostOptions} = require('../../helpers/api/apiHelper');
+const token = require("../../config/auth");
+const { apiPostOptions, apiPostMethod } = require("../../helpers/api/apiHelper");
 const req = require("request");
 
 module.exports = (fastify, opts, done) => {
-  fastify.post('/customers/auth', (request, reply) => {
+  fastify.post("/customers/auth", (request, reply) => {
+    req(apiPostOptions("/customers/auth", null, request.body), function(
+      error,
+      response
+    ) {
+      if (error) throw new Error(error);
+      token.set(response.headers.authorization.split(" ")[1]);
+      reply.code(200).send(response.headers);
+      return response;
+    });
+  });
 
-  req(apiPostOptions('/customers/auth', null ,request.body), function(error, response) {
-    if (error) throw new Error(error);
-    token.set(response.headers.authorization.split(' ')[1])
-    reply.code(200).send(response.headers);
-    return response;
-  })
-  })
-  done()
-}
+  fastify.post("/sessions", (request, reply) => {
+    apiPostMethod(
+      `/sessions`,
+      "",
+      '',
+      request.headers.authorization
+    )
+      .then(response => reply.code(200).send(response.headers['set-cookie']))
+      .catch(err => reply.code(500).send(err.response));
+  });
+  done();
+};
